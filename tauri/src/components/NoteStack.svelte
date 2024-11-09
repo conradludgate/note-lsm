82e8ef89d7a82861e62c7344c10e71479516d3be
Conrad Ludgate <conradludgate@gmail.com>
2024-11-09 18:50:13 +0000

    start on ui

---
+++ tauri/src/components/NoteStack.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/core";
+</script>
+
+<div class="notestack">
+  <div><p>foo bar</p></div>
+  <div><p>foo bar</p></div>
+  <div><p>foo bar</p></div>
+  <div><p>foo bar</p></div>
+</div>
+
+<style>
+  .notestack {
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
