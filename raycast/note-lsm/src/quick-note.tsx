2a84bc8beeb4e5c9863331bc929d3370918fa0cc
Conrad Ludgate <conradludgate@gmail.com>
2024-11-09 12:46:28 +0000

    remove tagging

--- raycast/note-lsm/src/quick-note.tsx
+++ raycast/note-lsm/src/quick-note.tsx
@@ -1,11 +1,8 @@
 import { Form, ActionPanel, Action, popToRoot, LaunchProps, getPreferenceValues } from "@raycast/api";
-import { useExec, useFrecencySorting } from "@raycast/utils";
 import { execFile } from "child_process";
-import { useMemo } from "react";
 import { promisify } from "util";
 
 interface QuickNoteDraft {
-  tags: string[],
 }
 
 interface QuickNoteArgs {
@@ -16,42 +13,18 @@ interface Preferences {
   cmdPath: string,
 }
 
-interface Tag {
-  tag: string,
-  description: string | null,
-}
-
 export default function QuickNote(props: LaunchProps<{ draftValues: QuickNoteDraft, arguments: QuickNoteArgs }>) {
   const { cmdPath } = getPreferenceValues<Preferences>();
-  const { isLoading, data } = useExec(cmdPath, ["--output=json", "tags", "list"]);
-
-  const tags = useMemo<Tag[]>(() => {
-    const tags: Tag[] = JSON.parse(data || "[]");
-    // return tags.map(tag => ({ id: tag.tag, description: tag.description }));
-    return tags
-  }, [data]);
-  const { data: sortedTags, visitItem } = useFrecencySorting<Tag>(tags, {
-    key: (tag) => tag.tag
-  });
-
-  const { draftValues, arguments: { note } } = props;
+  const { arguments: { note } } = props;
 
   return (
     <Form
-      isLoading={isLoading}
       enableDrafts
       actions={
         <ActionPanel>
           <Action.SubmitForm
-            onSubmit={async (values: QuickNoteDraft) => {
-              await Promise.all(values.tags.map(tag => visitItem(JSON.parse(tag))));
-
-              const tags = values.tags.flatMap((tag) => {
-                const t: Tag = JSON.parse(tag);
-                return ["--tag", t.tag]
-              });
-
-              const { stdout, stderr } = await promisify(execFile)(cmdPath, ["record", ...tags, note]);
+            onSubmit={async () => {
+              const { stdout, stderr } = await promisify(execFile)(cmdPath, ["record", note]);
               console.log({ stdout, stderr });
 
               await popToRoot();
@@ -60,13 +33,6 @@ export default function QuickNote(props: LaunchProps<{ draftValues: QuickNoteDra
         </ActionPanel>
       }
     >
-      <Form.TagPicker id="tags" title="Tags" defaultValue={draftValues?.tags}>
-        {sortedTags?.map((tag) => <Form.TagPicker.Item
-          key={tag.tag}
-          value={JSON.stringify(tag)}
-          title={tag.description ?? tag.tag}
-        />)}
-      </Form.TagPicker>
     </Form>
   );
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use atuin_client::{
 };
 use atuin_common::record::{DecryptedData, Host, Record};
 use clap::Parser;
-use comfy_table::Table;
+// use comfy_table::Table;
 use serde::{Deserialize, Serialize};
 
 #[derive(clap::Parser, Debug)]
@@ -23,30 +23,11 @@ struct Args {
 
 #[derive(clap::Subcommand, Debug)]
 enum Command {
-    Tags(TagsArgs),
     Record(RecordArgs),
 }
 
 #[derive(clap::Parser, Debug)]
-struct TagsArgs {
-    #[command(subcommand)]
-    command: TagsCommand,
-}
-
-#[derive(clap::Subcommand, Debug)]
-enum TagsCommand {
-    List,
-    Add {
-        tag: String,
-        description: Option<String>,
-    },
-}
-
-#[derive(clap::Parser, Debug)]
 struct RecordArgs {
-    #[arg(short, long = "tag")]
-    tags: Vec<String>,
-
     note: String,
 }
 
@@ -70,49 +51,8 @@ async fn main() {
     let key = encryption::load_key(&settings).unwrap().into();
 
     let output: Box<dyn EncodeOutput> = match args.command {
-        Command::Tags(tags_args) => match tags_args.command {
-            TagsCommand::List => {
-                let tags = sqlite_store
-                    .all_tagged("notes_lsm::tag")
-                    .await
-                    .unwrap()
-                    .into_iter()
-                    .filter(|encrypted| encrypted.version == "v0-alpha0")
-                    .map(|encrypted| encrypted.decrypt::<PASETO_V4>(&key).unwrap())
-                    .map(|decrypted| serde_json::from_slice(&decrypted.data.0).unwrap())
-                    .collect();
-
-                Box::new(TagsListOutput { tags })
-            }
-            TagsCommand::Add { tag, description } => {
-                println!("adding tag {tag:?} with description {description:?}",);
-
-                let idx = sqlite_store
-                    .last(host_id, "notes_lsm::tag")
-                    .await
-                    .unwrap()
-                    .map_or(0, |p| p.idx + 1);
-
-                let record = Record::builder()
-                    .data(DecryptedData(
-                        serde_json::to_vec(&Tag { tag, description }).unwrap(),
-                    ))
-                    .tag("notes_lsm::tag".to_string())
-                    .idx(idx)
-                    .host(Host::new(host_id))
-                    .version("v0-alpha0".to_string())
-                    .build();
-                let record = record.encrypt::<PASETO_V4>(&key);
-                sqlite_store.push(&record).await.unwrap();
-
-                Box::new(NoOutput {})
-            }
-        },
         Command::Record(record_args) => {
-            println!(
-                "adding {:?} with tags {:?}",
-                record_args.note, record_args.tags
-            );
+            println!("adding {:?}", record_args.note);
 
             let idx = sqlite_store
                 .last(host_id, "notes_lsm::record")
@@ -124,14 +64,13 @@ async fn main() {
                 .data(DecryptedData(
                     serde_json::to_vec(&NoteLeaf {
                         note: record_args.note,
-                        tags: record_args.tags,
                     })
                     .unwrap(),
                 ))
                 .tag("notes_lsm::record".to_string())
                 .idx(idx)
                 .host(Host::new(host_id))
-                .version("v0-alpha0".to_string())
+                .version("v0-alpha1".to_string())
                 .build();
             let record = record.encrypt::<PASETO_V4>(&key);
             sqlite_store.push(&record).await.unwrap();
@@ -146,27 +85,6 @@ async fn main() {
 
 trait EncodeOutput {
     fn encode(self: Box<Self>, method: Output, w: &mut dyn io::Write) -> io::Result<()>;
-}
-
-struct TagsListOutput {
-    tags: Vec<Tag>,
-}
-
-impl EncodeOutput for TagsListOutput {
-    fn encode(self: Box<Self>, method: Output, w: &mut dyn io::Write) -> io::Result<()> {
-        match method {
-            Output::Text => {
-                let mut table = Table::new();
-                table.set_header(["tag", "description"]).add_rows(
-                    self.tags
-                        .into_iter()
-                        .map(|tag| [tag.tag, tag.description.unwrap_or_default()]),
-                );
-                writeln!(w, "{table}")
-            }
-            Output::Json => json(&self.tags, w),
-        }
-    }
 }
 
 struct NoOutput {}
@@ -201,5 +119,4 @@ fn json_io_error(e: serde_json::Error) -> io::Error {
 #[derive(Serialize, Deserialize)]
 struct NoteLeaf {
     note: String,
-    tags: Vec<String>,
 }
